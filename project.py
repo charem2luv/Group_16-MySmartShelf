e0e680eaccdb311e4021edcc15ccdb27ae7f731a
amen2x <amengirmawi@gmail.com>
2025-06-01 11:41:07 +0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,15 @@
+book_collection = {
+    "to_read": set("Atomic Habits,Confession,HTML".split(",")),
+    "read": set("Rich dad Poor dad,Go pro,python for beginners".split(","))
+}
+
+def add_book(title):
+    if title in book_collection["to_read"] or title in book_collection["read"]:
+        print(f"'{title}' is already in your list.")
+    else:
+        book_collection["to_read"].add(title)
+        print(f"'{title}' added to your reading list.")
+
+def edit_book(old_title, new_title):
+    if old_title in book_collection["to_read"]:
+        book_collection["to_read"].remove(old_title)
